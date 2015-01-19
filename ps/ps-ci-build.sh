7dec93c99f05af682d52f560fc46616433fa6fa1
Leo Yuriev <leo@yuriev.ru>
2015-01-19 09:08:00 +0300

    ps-build: enable most of all configure's features for ci-build.
    Change-Id: I00c5b8161a0d0bd38c6a3dd81f4e3ce97480fdae

--- ps/ps-ci-build.sh
+++ ps/ps-ci-build.sh
@@ -8,7 +8,14 @@ failure() {
 }
 
 git clean -x -f -d -e .ccache/ -e tests/testrun/ -e times.log || failure "cleanup"
-CFLAGS="-Wall -g -Os" CPPFLAGS="-Wall -g -Os" ./configure || failure "configure"
+
+CFLAGS="-Wall -g -Os -DLDAP_MEMORY_DEBUG -DSLAP_NO_SL_MALLOC -DUSE_VALGRIND" CPPFLAGS="$CFLAGS" \
+	./configure \
+		--enable-backends --enable-overlays \
+		--enable-rewrite --enable-dynacl --enable-aci --enable-slapi \
+		--disable-dependency-tracking \
+	|| failure "configure"
+
 make -j4 && make -j4 -C libraries/liblmdb || failure "build"
 
 echo "DONE"
