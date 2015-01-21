1c8894ae33f01e0afcb81a1e1e1589e6bef46095
Leo Yuriev <leo@yuriev.ru>
2015-01-21 07:50:14 +0300

    liblber: added "Hipagut" memory checker (imported partialy from 1Hippeus).
    Change-Id: I578a61b4eeaa0e8f312f3ba36088510beec2c97c

---
+++ include/lber_hipagut.h
@@ -0,0 +1,268 @@
+/*
+	Imported from 1Hippeus project at 2015-01-19.
+
+	Copyright (c) 2013 Leonid Yuriev <leo@yuriev.ru>.
+    Copyright (c) 2013 Peter-Service LLC.
+    Copyright (c) 2013 Other contributors as noted in the AUTHORS file.
+
+    This file is part of 1Hippeus.
+
+    1Hippeus is free software; you can redistribute it and/or modify it under
+    the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    1Hippeus is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _LBER_HUG_H
+#define _LBER_HUG_H
+
+#include <stddef.h>
+#include <ldap_cdefs.h>
+
+/* ----------------------------------------------------------------------------
+ * LY: Originally calles "Hipagut" = sister in law, nurse
+ * (Tagalog language, Philippines). */
+
+LDAP_BEGIN_DECL
+
+struct lber_hipagut {
+	char opacue[8];
+};
+typedef struct lber_hipagut lber_hug_t;
+
+LBER_F(void) lber_hug_setup LDAP_P((
+	lber_hug_t*,
+	const unsigned n42 ));
+
+LBER_F(void) lber_hug_drown LDAP_P((
+	lber_hug_t* ));
+
+/* Return zero when no corruption detected, otherwise returns -1. */
+LBER_F(int) lber_hug_probe LDAP_P((
+	const lber_hug_t*,
+	const unsigned n42 ));
+
+LBER_F(void) lber_hug_setup_link LDAP_P((
+	lber_hug_t* slave,
+	const lber_hug_t* master ));
+
+LBER_F(void) lber_hug_drown_link LDAP_P((
+	lber_hug_t* slave,
+	const lber_hug_t* master ));
+
+/* Return zero when no corruption detected, otherwise returns -1. */
+LBER_F(int) lber_hug_probe_link LDAP_P((
+	const lber_hug_t* slave,
+	const lber_hug_t* master ));
+
+#define LBER_HUG_TETRAD(a, b, c, d) \
+	((a) << 24 | (b) << 16 | (c) << 8 | (d))
+
+#define LBER_HUG_N42(label) \
+	LBER_HUG_TETRAD(# label[0], # label[1], # label[2], # label[3])
+
+#define LBER_HUG_DECLARE(gizmo) \
+	struct lber_hipagut gizmo
+
+/* -------------------------------------------------------------------------- */
+
+#define LBER_HUG_SETUP(gizmo, label) \
+	lber_hug_setup(&(gizmo), LBER_HUG_N42(label))
+
+#define LBER_HUG_DROWN(gizmo) \
+	lber_hug_drown(&(gizmo))
+
+#define LBER_HUG_PROBE(gizmo, label) \
+	lber_hug_probe(&(gizmo), LBER_HUG_N42(label))
+
+#define LBER_HUG_SETUP_LINK(slave, master) \
+	lber_hug_setup_link(&(slave), &(master))
+
+#define LBER_HUG_PROBE_LINK(slave, master) \
+	lber_hug_probe_link(&(slave), &(master))
+
+/* -------------------------------------------------------------------------- */
+
+#define LBER_HUG_SPACE \
+	((ptrdiff_t)sizeof(lber_hug_t))
+
+#define LBER_HUG_ASIDE(base, offset) \
+	((lber_hug_t*)(((char*)(base)) + (offset)))
+
+#define LBER_HUG_BEFORE(address) \
+	LBER_HUG_ASIDE(address, -LBER_HUG_SPACE)
+
+/* -------------------------------------------------------------------------- */
+
+#define LBER_HUG_SETUP_ASIDE(base, label, offset) \
+	lber_hug_setup(LBER_HUG_ASIDE(base, offset), LBER_HUG_N42(label))
+
+#define LBER_HUG_DROWN_ASIDE(base, offset) \
+	lber_hug_drown(LBER_HUG_ASIDE(base, offset))
+
+#define LBER_HUG_PROBE_ASIDE(base, label, offset) \
+	lber_hug_probe(LBER_HUG_ASIDE(base, offset), LBER_HUG_N42(label))
+
+#define LBER_HUG_SETUP_LINK_ASIDE(base, master, offset) \
+	lber_hug_setup_link(LBER_HUG_ASIDE(base, offset), &(master))
+
+#define LBER_HUG_PROBE_LINK_ASIDE(base, master, offset) \
+	lber_hug_probe_link(LBER_HUG_ASIDE(base, offset), &(master))
+
+/* -------------------------------------------------------------------------- */
+
+#ifndef likely
+#	if defined(__GNU_C) || defined(__clang__)
+#		ifdef __cplusplus
+			/* LY: workaround for "pretty" boost */
+			static __inline __attribute__((always_inline))
+				bool likely(bool cond) { return __builtin_expect(cond, 1); }
+#		else
+#			define likely(cond) __builtin_expect(!!(cond), 1)
+#		endif
+#	else
+#		define likely(x) (x)
+#	endif
+#endif /* likely */
+
+#ifndef unlikely
+#	if defined(__GNU_C) || defined(__clang__)
+#		ifdef __cplusplus
+			/* LY: workaround for "pretty" boost */
+			static __inline __attribute__((always_inline))
+				bool unlikely(bool cond) { return __builtin_expect(cond, 0); }
+#		else
+#			define unlikely(cond) __builtin_expect(!!(cond), 0)
+#		endif
+#	else
+#		define unlikely(x) (x)
+#	endif
+#endif /* unlikely */
+
+/* Prototype should match libc runtime. ISO POSIX (2003) & LSB 3.1 */
+void __assert_fail LDAP_P((
+	const char* assertion,
+	const char* file,
+	unsigned line,
+	const char* function ));
+
+#define lber_hug_throw(info) \
+	__assert_fail("hipagut: guard " info, __FILE__, __LINE__, __FUNCTION__)
+
+#define LBER_HUG_ENSURE(gizmo, label) do \
+	if (unlikely(! LBER_HUG_PROBE(gizmo, label))) \
+			lber_hug_throw(#label "@" #gizmo); \
+    while (0)
+
+#define LBER_HUG_ENSURE_ASIDE(base, label, offset) \
+	if (unlikely(! LBER_HUG_PROBE_ASIDE(base, label, offset))) \
+		lber_hug_throw(#label "@" #base "[" #offset "]"); \
+    while (0)
+
+#define LBER_HUG_ENSURE_LINK(slave, master) do \
+	if (unlikely(! LBER_HUG_PROBE_LINK(slave, master))) \
+			lber_hug_throw(#master "~" #slave); \
+    while (0)
+
+#define LBER_HUG_ENSURE_LINK_ASIDE(base, master, offset) \
+	if (unlikely(! LBER_HUG_PROBE_LINK_ASIDE(base, master, offset))) \
+		lber_hug_throw(#master "~" #base "[" #offset "]"); \
+    while (0)
+
+#if HIPAGUT_ASSERT_CHECK
+#   define LBER_HUG_ASSERT(gizmo, label)  \
+		LBER_HUG_ENSURE(gizmo, label)
+#   define LBER_HUG_ASSERT_ASIDE(base, label, offset) \
+		LBER_HUG_ENSURE_ASIDE(base, label, offset)
+#   define LBER_HUG_ASSERT_LINK(slave, maser) \
+		LBER_HUG_ENSURE_LINK(slave, master)
+#   define LBER_HUG_ASSERT_LINK_ASIDE(base, master, offset) \
+		LBER_HUG_ENSURE_LINK_ASIDE(base, master, offset)
+#else
+#   define LBER_HUG_ASSERT(gizmo, label) \
+		do {} while (0)
+#   define LBER_HUG_ASSERT_ASIDE(base, label, offset) \
+		do {} while (0)
+#   define LBER_HUG_ASSERT_LINK(slave, master) \
+		do {} while (0)
+#   define LBER_HUG_ASSERT_LINK_ASIDE(base, master, offset) \
+		do {} while (0)
+#endif /* HIPAGUT_ASSERT_CHECK */
+
+/* -------------------------------------------------------------------------- */
+
+#define LBER_HUG_DISABLED 0xfea51b1eu /* feasible */
+LBER_V(unsigned) lber_hug_nasty_disabled;
+LBER_V(unsigned) lber_hug_memchk_poison_alloc;
+LBER_V(unsigned) lber_hug_memchk_poison_free;
+LBER_V(unsigned) lber_hug_memchk_trace_disabled;
+
+struct _lber_hug_memchk_info {
+	volatile size_t mi_inuse_bytes;
+	volatile size_t mi_inuse_chunks;
+	volatile size_t mi_sequence;
+};
+
+LBER_V(struct _lber_hug_memchk_info) lber_hug_memchk_info;
+
+struct lber_hug_memchk {
+	LBER_HUG_DECLARE(hm_guard_head);
+	size_t hm_length;
+	size_t hm_sequence; /* Allocation sequence number */
+	LBER_HUG_DECLARE(hm_guard_bottom);
+};
+
+#define LBER_HUG_MEMCHK_OVERHEAD \
+	(sizeof(struct lber_hug_memchk) + sizeof(struct lber_hipagut))
+
+#define LBER_HUG_CHUNK(ptr) \
+	((struct lber_hug_memchk*) \
+		(((char*) (ptr)) - sizeof(struct lber_hug_memchk)))
+
+#define LBER_HUG_PAYLOAD(memchunk) \
+	((void*) \
+		(((char*) (memchunk)) + sizeof(struct lber_hug_memchk)))
+
+/* Defines for 'poison_mode' of lber_hug_memchk_setup() */
+#define LBER_HUG_POISON_DEFAULT		0xDEFA0178u
+#define LBER_HUG_POISON_CALLOC		0xCA110C00u
+#define LBER_HUG_POISON_DISABLED	LBER_HUG_DISABLED
+
+LBER_F(void*) lber_hug_memchk_setup LDAP_P((
+	struct lber_hug_memchk* memchunk,
+	size_t payload_size,
+	unsigned poison_mode ));
+
+LBER_F(void*) lber_hug_memchk_drown LDAP_P(( void* payload ));
+LBER_F(void) lber_hug_memchk_ensure LDAP_P(( const void* payload ));
+LBER_F(size_t) lber_hug_memchk_size LDAP_P(( const void* payload ));
+
+LBER_F(unsigned) lber_hug_realloc_begin LDAP_P((
+	const void* payload,
+	size_t* old_size ));
+LBER_F(void*) lber_hug_realloc_undo LDAP_P((
+	struct lber_hug_memchk* old_memchunk,
+	unsigned undo_key ));
+LBER_F(void*) lber_hug_realloc_commit LDAP_P((
+	size_t old_size,
+	struct lber_hug_memchk* new_memchunk,
+	size_t new_size ));
+
+/* Return zero when no corruption detected,
+ * otherwise returns combination of OR'ed bits: 1, 2, 3. */
+LBER_F(int) lber_hug_memchk_probe LDAP_P((
+	const void* payload,
+	size_t *length,
+	size_t *sequence ));
+
+LDAP_END_DECL
+
+#endif /* _LBER_HUG_H */
---
+++ libraries/liblber/hipagut.c
@@ -0,0 +1,527 @@
+/*
+	Imported from 1Hippeus project at 2015-01-19.
+
+	Copyright (c) 2013 Leonid Yuriev <leo@yuriev.ru>.
+	Copyright (c) 2014 Peter-Service R&D LLC.
+
+    This file is part of 1Hippeus.
+
+    1Hippeus is free software; you can redistribute it and/or modify it under
+    the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    1Hippeus is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <time.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "lber_hipagut.h"
+#include "portable.h"
+
+/* LY: only for self debugging
+#include <stdio.h> */
+
+/* -------------------------------------------------------------------------- */
+
+#ifndef __forceinline
+#	if defined(__GNU_C) || defined(__clang__)
+#		define __forceinline __inline __attribute__((always_inline))
+#	elif ! defined(_MSC_VER)
+#		define __forceinline
+#	endif
+#endif /* __forceinline */
+
+#ifndef __must_check_result
+#	if defined(__GNU_C) || defined(__clang__)
+#		define __must_check_result __attribute__((warn_unused_result))
+#	else
+#		define __must_check_result
+#	endif
+#endif /* __must_check_result */
+
+#ifndef __hot
+#	if defined(__GNU_C) || defined(__clang__)
+#		define __hot __attribute__((hot, optimize("O3")))
+#	else
+#		define __hot
+#	endif
+#endif /* __hot */
+
+#ifndef __flatten
+#	if defined(__GNU_C) || defined(__clang__)
+#		define __flatten __attribute__((flatten))
+#	else
+#		define __flatten
+#	endif
+#endif /* __flatten */
+
+#ifdef USE_VALGRIND
+		/* Get debugging help from Valgrind */
+#       include <valgrind/memcheck.h>
+#else
+#       define VALGRIND_CREATE_MEMPOOL(h,r,z)
+#       define VALGRIND_DESTROY_MEMPOOL(h)
+#       define VALGRIND_MEMPOOL_TRIM(h,a,s)
+#       define VALGRIND_MEMPOOL_ALLOC(h,a,s)
+#       define VALGRIND_MEMPOOL_FREE(h,a)
+#       define VALGRIND_MEMPOOL_CHANGE(h,a,b,s)
+#       define VALGRIND_MAKE_MEM_NOACCESS(a,s)
+#       define VALGRIND_MAKE_MEM_DEFINED(a,s)
+#       define VALGRIND_MAKE_MEM_UNDEFINED(a,s)
+#       define VALGRIND_DISABLE_ADDR_ERROR_REPORTING_IN_RANGE(a,s)
+#       define VALGRIND_ENABLE_ADDR_ERROR_REPORTING_IN_RANGE(a,s)
+#endif
+
+/* -------------------------------------------------------------------------- */
+
+#ifndef CACHELINE_SIZE
+#	define CACHELINE_SIZE 64
+#endif
+
+static __forceinline uint64_t unaligned_load(const volatile void* ptr) {
+#if defined(__x86_64__) || defined(__i386__)
+	return *(const volatile uint64_t*) ptr;
+#else
+	uint64_t local;
+#	if defined(__GNUC__) || defined(__clang__)
+		__builtin_memcpy(&local, ptr, 8);
+#	else
+		memcpy(&local, ptr, 8);
+#	endif /* __GNUC__ || __clang__ */
+	return local;
+#endif /* arch selector */
+}
+
+static __forceinline void unaligned_store(volatile void* ptr, uint64_t value) {
+#if defined(__x86_64__) || defined(__i386__)
+	*(volatile uint64_t*) ptr = value;
+#else
+#	if defined(__GNUC__) || defined(__clang__)
+		__builtin_memcpy(ptr, &value, ptr, 8);
+#	else
+		memcpy(ptr, &value, 8);
+#	endif /* __GNUC__ || __clang__ */
+#endif /* arch selector */
+}
+
+#if defined(__MSC_VER)
+	__declspec(align(CACHELINE_SIZE))
+#endif /* _MSC_VER */
+struct _lber_hug_memchk_info
+#if defined(__GNU_C) || defined(__clang__)
+		__attribute__((aligned(CACHELINE_SIZE)))
+#endif /* __GNUC__ || __clang__ */
+	lber_hug_memchk_info;
+
+#if defined(__GNUC__) || defined(__clang__)
+	/* LY: noting needed */
+#elif defined(_MSC_VER)
+#	define __sync_fetch_and_add(p,v) _InterlockedExchangeAdd(p,v)
+#	define __sync_fetch_and_sub(p,v) _InterlockedExchangeAdd(p,-(v))
+#elif defined(__APPLE__)
+#	include <libkern/OSAtomic.h>
+	static size_t __sync_fetch_and_add(volatile size_t *p, size_t v) {
+		switch(sizeof(size_t)) {
+		case 4:
+			return OSAtomicAdd32(v, (volatile int32_t*) p);
+		case 8:
+			return OSAtomicAdd64(v, (volatile int64_t*) p);
+		default: {
+			size_t snap = *p; *p += v; return snap;
+		}
+	}
+	#	define __sync_fetch_and_sub(p,v) __sync_fetch_and_add(p,-(v))
+#else
+	static size_t __sync_fetch_and_add(volatile size_t *p, size_t v) {
+		size_t snap = *p; *p += v; return snap;
+	}
+#	define __sync_fetch_and_sub(p,v) __sync_fetch_and_add(p,-(v))
+#endif
+
+static __forceinline uint64_t entropy_ticks() {
+#if defined(__GNUC__) || defined(__clang__)
+#if defined(__ia64__)
+	uint64_t ticks;
+	__asm("mov %0=ar.itc" : "=r" (ticks));
+	return ticks;
+#elif defined(__hppa__)
+	uint64_t ticks;
+	__asm("mfctl 16, %0" : "=r" (ticks));
+	return ticks;
+#elif defined(__s390__)
+	uint64_t ticks;
+	__asm("stck 0(%0)" : : "a" (&(ticks)) : "memory", "cc");
+	return ticks;
+#elif defined(__alpha__)
+	uint64_t ticks;
+	__asm("rpcc %0" : "=r" (ticks));
+	return ticks;
+#elif defined(__sparc_v9__)
+	uint64_t ticks;
+	__asm("rd %%tick, %0" : "=r" (ticks));
+	return ticks;
+#elif defined(__ppc__) || defined(__powerpc__)
+	unsigned tbl, tbu0, tbu1;
+
+	do {
+		__asm("mftbu %0" : "=r" (tbu0));
+		__asm("mftb %0" : "=r" (tbl));
+		__asm("mftbu %0" : "=r" (tbu1));
+	} while (unlikely(tbu0 != tbu1));
+
+	return (((uin64_t) tbu0) << 32) | tbl;
+#elif defined(__x86_64__) || defined(__i386__)
+	unsigned lo, hi;
+
+	/* LY: Using the "a" and "d" constraints is important for correct code. */
+	__asm("rdtsc" : "=a" (lo), "=d" (hi));
+
+	return (((uint64_t)hi) << 32) + lo;
+#endif /* arch selector */
+#endif /* __GNUC__ || __clang__ */
+
+	static volatile size_t sequence;
+	uint64_t salt = __sync_fetch_and_add(&sequence, 798021487u);
+	return ((salt << 17) ^ (salt >> 5)) * 3681268039ul;
+}
+
+/* -------------------------------------------------------------------------- */
+
+static __forceinline unsigned canary() {
+	uint64_t ticks = entropy_ticks();
+    return ticks ^ (ticks >> 32);
+}
+
+static __forceinline uint32_t linear_congruential(uint32_t value) {
+	return value * 1664525u + 1013904223u;
+}
+
+/*! Calculate magic mesh from a variable chirp and the constant salt n42 */
+static __forceinline uint32_t mixup(unsigned chirp, const unsigned n42) {
+	uint64_t caramba;
+
+	/* LY: just multiplication by a prime number. */
+	if (sizeof(unsigned long) > 4)
+		caramba = 1445174711lu * (unsigned long)(chirp + n42);
+	else {
+#if (defined(__GNUC__) || defined(__clang__)) && defined(__i386__)
+		__asm("mull %2" : "=A" (caramba) : "a" (1445174711u), "r" (chirp + n42) : "cc");
+#else
+		caramba = ((uint64_t) chirp + n42) * 1445174711u;
+#endif
+	}
+
+	/* LY: significally less collisions when only the few bits damaged. */
+	return caramba ^ (caramba >> 32);
+}
+
+static __forceinline int fairly(uint32_t value) {
+	if (unlikely((value >= 0xFFFF0000u) || (value <= 0x0000FFFFu)))
+		return 0;
+
+	return 1;
+}
+
+__hot __flatten void lber_hug_setup(lber_hug_t* self, const unsigned n42) {
+	uint32_t chirp = canary();
+	for (;;) {
+		if (likely(fairly(chirp))) {
+			uint32_t sign = mixup(chirp, n42);
+			if (likely(fairly(sign))) {
+				uint64_t tale = chirp + (((uint64_t)sign) << 32);
+				/* fprintf(stderr, "hipagut_setup: ptr %p | n42 %08x, "
+						"tale %016lx, chirp %08x, sign %08x ? %08x\n",
+						self, n42, tale, chirp, sign, mixup(chirp, n42)); */
+				unaligned_store(self->opacue, tale);
+				break;
+			}
+		}
+		chirp = linear_congruential(chirp);
+	}
+}
+
+__hot __flatten int lber_hug_probe(const lber_hug_t* self, const unsigned n42) {
+	if (unlikely(lber_hug_nasty_disabled == LBER_HUG_DISABLED))
+		return 0;
+
+	uint64_t tale = unaligned_load(self->opacue);
+	uint32_t chirp = tale;
+	uint32_t sign = tale >> 32;
+	/* fprintf(stderr, "hipagut_probe: ptr %p | n42 %08x, "
+			"tale %016lx, chirp %08x, sign %08x ? %08x\n",
+			self, n42, tale, chirp, sign, mixup(chirp, n42)); */
+	return likely(fairly(chirp) && fairly(sign)
+				  && sign == mixup(chirp, n42)) ? 0 : -1;
+}
+
+__hot __flatten void lber_hug_drown(lber_hug_t* gizmo) {
+	/* LY: This notable value would always bite,
+	 * because (chirp == 0) != (sign == 0). */
+	unaligned_store(gizmo->opacue, 0xDEADB0D1Ful);
+}
+
+void lber_hug_setup_link(lber_hug_t* slave, const lber_hug_t* master) {
+	lber_hug_setup(slave, unaligned_load(master->opacue) >> 32);
+}
+
+void lber_hug_drown_link(lber_hug_t* slave, const lber_hug_t* master) {
+	lber_hug_drown(slave);
+}
+
+int lber_hug_probe_link(const lber_hug_t* slave, const lber_hug_t* master) {
+	return lber_hug_probe(slave, unaligned_load(master->opacue) >> 32);
+}
+
+/* -------------------------------------------------------------------------- */
+
+#define MEMCHK_TAG_HEADER	header
+#define MEMCHK_TAG_BOTTOM	bottom
+#define MEMCHK_TAG_COVER	cover
+
+#define MEMCHK_TAG_HEADER_REALLOC	_Header
+#define MEMCHK_TAG_BOTTOM_REALLOC	_Bottom
+#define MEMCHK_TAG_COVER_REALLOC	_Cover
+
+static void lber_hug_memchk_throw(const void* payload, unsigned bits) {
+	const char* trouble;
+	switch (bits) {
+	case 0:
+		return;
+	case 1:
+		trouble = "hipagut: corrupted memory chunk"
+				  " (likely overruned by other)";
+		break;
+	case 2:
+		trouble = "hipagut: corrupted memory chunk"
+				  " (likely underrun)";
+		break;
+	case 4:
+		trouble = "hipagut: corrupted memory chunk"
+				  " (likely overrun)";
+		break;
+	/* case 3: */
+	default:
+		trouble = "hipagut: corrupted memory chunk"
+				  " (control header was destroyed)";
+	}
+	__assert_fail(trouble, __FILE__, __LINE__, __FUNCTION__);
+}
+
+__hot __flatten size_t lber_hug_memchk_size(const void* payload) {
+	size_t size = 0;
+	unsigned bits = lber_hug_memchk_probe(payload, &size, NULL);
+
+	if (unlikely (bits != 0))
+		lber_hug_memchk_throw(payload, bits);
+
+	return size;
+}
+
+#define VALGRIND_CLOSE(memchunk) { \
+		VALGRIND_MAKE_MEM_NOACCESS((char *) memchunk \
+			+ memchunk->hm_length + sizeof(*memchunk), \
+				sizeof(*memchunk)); \
+		VALGRIND_MAKE_MEM_NOACCESS(memchunk, sizeof(*memchunk)); \
+	}
+
+#define VALGRIND_OPEN(memchunk) { \
+		VALGRIND_MAKE_MEM_DEFINED(memchunk, sizeof(*memchunk)); \
+		VALGRIND_MAKE_MEM_DEFINED((char *) memchunk \
+			+ memchunk->hm_length + sizeof(*memchunk), \
+				sizeof(*memchunk)); \
+	}
+
+__hot __flatten int lber_hug_memchk_probe(
+		const void* payload,
+		size_t *length,
+		size_t *sequence ) {
+	struct lber_hug_memchk* memchunk = LBER_HUG_CHUNK(payload);
+	unsigned bits = 0;
+
+	VALGRIND_OPEN(memchunk);
+	if (likely(lber_hug_nasty_disabled != LBER_HUG_DISABLED)) {
+		if (unlikely(LBER_HUG_PROBE(memchunk->hm_guard_head, MEMCHK_TAG_HEADER)))
+			bits |= 1;
+		if (unlikely(LBER_HUG_PROBE(memchunk->hm_guard_bottom, MEMCHK_TAG_BOTTOM)))
+			bits |= 2;
+		if (likely(bits == 0)) {
+			if (length)
+				*length = memchunk->hm_length;
+			if (sequence)
+				*sequence = memchunk->hm_sequence;
+			if (unlikely(LBER_HUG_PROBE_ASIDE(memchunk, MEMCHK_TAG_COVER,
+					memchunk->hm_length + sizeof(struct lber_hug_memchk))))
+				bits |= 4;
+		}
+	}
+	VALGRIND_CLOSE(memchunk);
+	return bits;
+}
+
+__hot __flatten void lber_hug_memchk_ensure(const void* payload) {
+	unsigned bits = lber_hug_memchk_probe(payload, NULL, NULL);
+
+	if (unlikely (bits != 0))
+		lber_hug_memchk_throw(payload, bits);
+}
+
+unsigned lber_hug_nasty_disabled;
+unsigned lber_hug_memchk_poison_alloc	= 0xCC;
+unsigned lber_hug_memchk_poison_free	= 0xDD;
+unsigned lber_hug_memchk_trace_disabled
+#ifdef LDAP_MEMORY_TRACE
+	;
+#else
+	= LBER_HUG_DISABLED;
+#endif
+
+__hot __flatten void* lber_hug_memchk_setup(
+		struct lber_hug_memchk* memchunk,
+		size_t payload_size,
+		unsigned poison_mode) {
+	void* payload = LBER_HUG_PAYLOAD(memchunk);
+
+	size_t sequence = LBER_HUG_DISABLED;
+	if (unlikely(lber_hug_memchk_trace_disabled != LBER_HUG_DISABLED)) {
+		sequence = __sync_fetch_and_add(&lber_hug_memchk_info.mi_sequence, 1);
+		__sync_fetch_and_add(&lber_hug_memchk_info.mi_inuse_chunks, 1);
+		__sync_fetch_and_add(&lber_hug_memchk_info.mi_inuse_bytes, payload_size);
+	}
+
+	LBER_HUG_SETUP(memchunk->hm_guard_head, MEMCHK_TAG_HEADER);
+	memchunk->hm_length = payload_size;
+	memchunk->hm_sequence = sequence;
+	LBER_HUG_SETUP(memchunk->hm_guard_bottom, MEMCHK_TAG_BOTTOM);
+	LBER_HUG_SETUP_ASIDE(memchunk, MEMCHK_TAG_COVER,
+		payload_size + sizeof(struct lber_hug_memchk));
+	VALGRIND_CLOSE(memchunk);
+
+	if (poison_mode == LBER_HUG_POISON_DEFAULT)
+		poison_mode = lber_hug_memchk_poison_alloc;
+	if (poison_mode != LBER_HUG_DISABLED) {
+		memset(payload, (char) poison_mode, payload_size);
+		if (poison_mode != LBER_HUG_POISON_CALLOC)
+			VALGRIND_MAKE_MEM_UNDEFINED(payload, payload_size);
+	}
+	return payload;
+}
+
+__hot __flatten void* lber_hug_memchk_drown(void* payload) {
+	size_t payload_size;
+	struct lber_hug_memchk* memchunk;
+
+	lber_hug_memchk_ensure(payload);
+	memchunk = LBER_HUG_CHUNK(payload);
+	VALGRIND_OPEN(memchunk);
+	payload_size = memchunk->hm_length;
+	LBER_HUG_DROWN(memchunk->hm_guard_head);
+	/* memchunk->hm_length = ~memchunk->hm_length; */
+	LBER_HUG_DROWN(memchunk->hm_guard_bottom);
+	LBER_HUG_DROWN_ASIDE(memchunk,
+		payload_size + sizeof(struct lber_hug_memchk));
+
+	if (unlikely(lber_hug_memchk_trace_disabled != LBER_HUG_DISABLED)) {
+		__sync_fetch_and_sub(&lber_hug_memchk_info.mi_inuse_chunks, 1);
+		__sync_fetch_and_sub(&lber_hug_memchk_info.mi_inuse_bytes, payload_size);
+	}
+
+	if (lber_hug_memchk_poison_free != LBER_HUG_DISABLED)
+		memset(payload, (char) lber_hug_memchk_poison_free, payload_size);
+
+	VALGRIND_MAKE_MEM_NOACCESS(payload, payload_size);
+	return memchunk;
+}
+
+static int lber_hug_memchk_probe_realloc(
+		struct lber_hug_memchk* memchunk, unsigned key) {
+	unsigned bits = 0;
+
+	if (likely(lber_hug_nasty_disabled != LBER_HUG_DISABLED)) {
+		if (unlikely(lber_hug_probe(&memchunk->hm_guard_head, key)))
+			bits |= 1;
+		if (unlikely(lber_hug_probe(&memchunk->hm_guard_bottom, key + 1)))
+			bits |= 2;
+		if (likely(bits == 0)) {
+			if (unlikely(
+				lber_hug_probe(LBER_HUG_ASIDE(memchunk,
+					memchunk->hm_length + sizeof(struct lber_hug_memchk)),
+					key + 2)))
+				bits |= 4;
+		}
+	}
+	return bits;
+}
+
+unsigned lber_hug_realloc_begin ( const void* payload, size_t* old_size ) {
+	struct lber_hug_memchk* memchunk;
+	unsigned key = canary();
+
+	lber_hug_memchk_ensure(payload);
+	memchunk = LBER_HUG_CHUNK(payload);
+	VALGRIND_OPEN(memchunk);
+	*old_size = memchunk->hm_length;
+	LBER_HUG_SETUP(memchunk->hm_guard_head, key);
+	LBER_HUG_SETUP(memchunk->hm_guard_bottom, key + 1);
+	LBER_HUG_SETUP_ASIDE(memchunk, key + 2,
+		memchunk->hm_length + sizeof(struct lber_hug_memchk));
+
+	return key;
+}
+
+void* lber_hug_realloc_undo ( struct lber_hug_memchk* memchunk,
+		unsigned key) {
+	unsigned bits = lber_hug_memchk_probe_realloc(memchunk, key);
+
+	if (unlikely (bits != 0)) {
+		void* payload = LBER_HUG_PAYLOAD(memchunk);
+		lber_hug_memchk_throw(payload, bits);
+	}
+
+	LBER_HUG_SETUP(memchunk->hm_guard_head, MEMCHK_TAG_HEADER);
+	LBER_HUG_SETUP(memchunk->hm_guard_bottom, MEMCHK_TAG_BOTTOM);
+	LBER_HUG_SETUP_ASIDE(memchunk, MEMCHK_TAG_COVER,
+		memchunk->hm_length + sizeof(struct lber_hug_memchk));
+	VALGRIND_CLOSE(memchunk);
+
+	return LBER_HUG_PAYLOAD(memchunk);
+}
+
+void* lber_hug_realloc_commit ( size_t old_size,
+		struct lber_hug_memchk* new_memchunk,
+		size_t new_size ) {
+	void* new_payload = LBER_HUG_PAYLOAD(new_memchunk);
+	size_t sequence = LBER_HUG_DISABLED;
+	if (unlikely(lber_hug_memchk_trace_disabled != LBER_HUG_DISABLED)) {
+		sequence = __sync_fetch_and_add(&lber_hug_memchk_info.mi_sequence, 1);
+		__sync_fetch_and_add(&lber_hug_memchk_info.mi_inuse_bytes, new_size - old_size);
+	}
+
+	LBER_HUG_SETUP(new_memchunk->hm_guard_head, MEMCHK_TAG_HEADER);
+	new_memchunk->hm_length = new_size;
+	new_memchunk->hm_sequence = sequence;
+	LBER_HUG_SETUP(new_memchunk->hm_guard_bottom, MEMCHK_TAG_BOTTOM);
+	LBER_HUG_SETUP_ASIDE(new_memchunk, MEMCHK_TAG_COVER,
+		new_size + sizeof(struct lber_hug_memchk));
+	VALGRIND_CLOSE(new_memchunk);
+
+	if (new_size > old_size) {
+		if (lber_hug_memchk_poison_alloc != LBER_HUG_POISON_DEFAULT)
+			memset((char *) new_payload + old_size,
+			   (char) lber_hug_memchk_poison_alloc, new_size - old_size);
+		if (lber_hug_memchk_poison_alloc != LBER_HUG_POISON_CALLOC)
+			VALGRIND_MAKE_MEM_UNDEFINED(
+						(char *) new_payload + old_size, new_size - old_size);
+	}
+
+	return new_payload;
+}
