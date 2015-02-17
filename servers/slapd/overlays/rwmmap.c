e25944516848aa7dfe5bc9ab436f6e927a20e25d
Leo Yuriev <leo@yuriev.ru>
2015-02-17 21:49:50 +0300

    slapd: cleanup some 'rebus' and LTO-warning in rwm-overlay.
    Change-Id: I26cf109cbb92ab660973c9a32fb9cd7790f65397

--- servers/slapd/overlays/rwmmap.c
+++ servers/slapd/overlays/rwmmap.c
@@ -33,8 +33,14 @@
 #include "slap.h"
 #include "rwm.h"
 
-#undef ldap_debug	/* silence a warning in ldap-int.h */
-#include "../../../libraries/libldap/ldap-int.h"
+#if 0 /* LY: I can't guess wherefore is that in original OpenLDAP, but:
+	   *     - seems completely unneeded, e.g. 'rebus codestyle';
+	   *     - generates LTO-warning, because we get a two
+	   *       types of 'struct ldapoptions' with different size.
+	   */
+#	undef ldap_debug	/* silence a warning in ldap-int.h */
+#	include "../../../libraries/libldap/ldap-int.h"
+#endif /* OpenLDAP's rebus codestyle */
 
 int
 rwm_mapping_cmp( const void *c1, const void *c2 )
