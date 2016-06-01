3aebd3d5a8025409863c92bc8c0666d38ab8de56
Leo Yuriev <leo@yuriev.ru>
2016-06-01 03:22:17 +0300

    reopenldap: remove obsolete & unsupported parts.
    Removes items which are maintained by OpenLDAP,
    but NOT provides by ReOpenLDAP project:
     - RFC and drafts (docs).
     - the OpenLDAP Administrator's Guide (docs).
     - the ldapc++ library (contrib).
     - obsolete ldaptcl (contrib).
     - build/mkdep.aix, DB_CONFIG, memcmp.c, etc...
    
    This is partially for:
     - https://github.com/ReOpen/ReOpenLDAP/issues/26
     - https://github.com/ReOpen/ReOpenLDAP/issues/93
    
    Change-Id: Ic5f246a78d678ab760e4c81a616a1ab105a043b7

--- include/ac/string.h
+++ include/ac/string.h
@@ -19,36 +19,16 @@
 
 #include <string.h>
 
-#if LDAP_SAFEMEMCPY
-#	undef memcpy
-	__extern_C void* ber_memcpy_safe(void* dest, const void* src, size_t n);
-#	define memcpy ber_memcpy_safe
-#endif
-
 /* use ldap_pvt_strtok instead of strtok or strtok_r! */
 LDAP_F(char *) ldap_pvt_strtok LDAP_P(( char *str,
 	const char *delim, char **pos ));
 
-#ifndef HAVE_STRDUP
-	/* strdup() is missing, declare our own version */
-#	undef strdup
-#	define strdup(s) ber_strdup(s)
-#elif !defined(_WIN32)
-	/* some systems fail to declare strdup */
-	/* Windows does not require this declaration */
-	LDAP_LIBC_F(char *) (strdup)();
+#if LDAP_SAFEMEMCPY
+#	undef memcpy
+#	define memcpy ber_memcpy_safe
+	/* LY: memcpy with checking for overlap */
+	__extern_C void* ber_memcpy_safe(void* dest, const void* src, size_t n);
 #endif
-
-#ifdef NEED_MEMCMP_REPLACEMENT
-	int (lutil_memcmp)(const void *b1, const void *b2, size_t len);
-#define memcmp lutil_memcmp
-#endif
-
-void *(lutil_memrchr)(const void *b, int c, size_t n);
-/* GNU extension (glibc >= 2.1.91), only declared when defined(_GNU_SOURCE) */
-#if defined(HAVE_MEMRCHR) && defined(_GNU_SOURCE)
-#define lutil_memrchr(b, c, n) memrchr(b, c, n)
-#endif /* ! HAVE_MEMRCHR */
 
 #define STRLENOF(s)	(sizeof(s)-1)
 
