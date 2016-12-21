9ee818e0d5a3509d80e43b6976f79c9c5b297626
Howard Chu <hyc@openldap.org>
2016-12-21 14:39:47 +0000

    backend-mdb: backport - Fix its6794 test.
    HYC: Must NULL out indexing cursors when closing tool txn
    
    LY: Seems this already fixed in ReOpenLDAP by ca391cf1842f1df8f650d247658b92924ae376ad and related.
    
    Change-Id: I8deb107048d4412639c69d5f2fa48f94dbb20f95

--- servers/slapd/back-mdb/tools.c
+++ servers/slapd/back-mdb/tools.c
@@ -213,6 +213,14 @@ int mdb_tool_entry_close(
 		mdb_cursor_close( cursor );
 		cursor = NULL;
 	}
+	{
+		struct mdb_info *mdb = be->be_private;
+		if ( mdb ) {
+			int i;
+			for (i=0; i<mdb->mi_nattrs; i++)
+				mdb->mi_attrs[i]->ai_cursor = NULL;
+		}
+	}
 	if( mdb_tool_txn ) {
 		MDB_TOOL_IDL_FLUSH( be, mdb_tool_txn );
 		int rc = mdb_tool_txn_commit(be, mdb_tool_txn);
