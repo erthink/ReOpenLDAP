b57d82a427723ed377062fe227a56097f16a868a
Pavel Tsaregorodtsev <ixpict@gmail.com>
2017-05-11 20:15:03 +0300

    Enable some backends on build:   - mdbx   - hdbx   - perl   - relay   - ldap   - null   - password   - monitor   - sock

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,8 @@
 REOPENLDAP_RUN_CMD=${REOPENLDAP_RUN_CMD:=-4 -h ldap:/// -g ldap -u ldap -f /opt/reopenldap/etc/slapd.conf -d1}
 REOPENLDAP_SLAPD_PATH=${REOPENLDAP_SLAPD_PATH:=/opt/reopenldap/sbin/slapd}
 
-
 echo "Trying run ReOpenLDAP: ${REOPENLDAP_SLAPD_PATH} ${REOPENLDAP_RUN_CMD}"
+
+chown ldap.ldap -R /var/lib/reopenldap
 
 exec $REOPENLDAP_SLAPD_PATH $REOPENLDAP_RUN_CMD
