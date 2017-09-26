1b0cf64d6a28be844aecf3994289ae454966d886
Quanah Gibson-Mount <quanah@openldap.org>
2017-09-26 11:51:27 -0700

    slapd: LDAP_EXPERIMENTAL for LDAP_FEATURE_SUBORDINATE_SCOPE.
    LDAP_FEATURE_SUBORDINATE_SCOPE is from expired draft-sermersheim-ldap-subordinate-scope,
    leave behind LDAP_EXPERIMENTAL.
    
    Change-Id: Id14609b43c659baa227d381d2af80ca1bec2cfbe

--- servers/slapd/root_dse.c
+++ servers/slapd/root_dse.c
@@ -36,7 +36,9 @@ static struct berval	builtin_supportedFeatures[] = {
 	BER_BVC(LDAP_FEATURE_ABSOLUTE_FILTERS),		/* (&) and (|) search filters */
 	BER_BVC(LDAP_FEATURE_LANGUAGE_TAG_OPTIONS),	/* Language Tag Options */
 	BER_BVC(LDAP_FEATURE_LANGUAGE_RANGE_OPTIONS),	/* Language Range Options */
+#if LDAP_EXPERIMENTAL > 0
 	BER_BVC(LDAP_FEATURE_SUBORDINATE_SCOPE),	/* "children" search scope */
+#endif /* LDAP_EXPERIMENTAL */
 	BER_BVNULL
 };
 static struct berval	*supportedFeatures;
